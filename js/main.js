305cbca369e39982d1487908b7b46a2932a2a189
Jacek Pietrzak <jacekpietrzak@Jaceks-MBP.home>
2017-11-04 00:21:08 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ console.log("My name is " + myName);
 myAge ++; //increase number by 1. You can use myAge = myAge + 1; or myAge += 1;
 myAge +=1;
 
-console.log("My age next year will be " + myAge)
+console.log("My age next year will be " + myAge);
 
 var iLikeCheese = true; // using one "=" is to assign/reassign a variable
-iLikeCheese == true // using two "==" is used for comparisons
+iLikeCheese == true; // using two "==" is used for comparisons
