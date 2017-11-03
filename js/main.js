8d6e59ace9f1227f9d0ce5d438790993276dea84
Jacek Pietrzak <jacekpietrzak@Jaceks-MBP.home>
2017-11-04 00:18:43 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+this is multiline comment
+*/
+
+//this is single line comment
+
+var welcome_message = "This is a welcome message stored inside a variable";
+var myName;
+var myAge = 31;
+var exactAge = 31.8;
+
+
+alert(welcome_message + " " + (myAge + exactAge));
+
+console.log("My age is " + myAge);
+myName = "Jacek";
+console.log("My name is " + myName);
+myAge ++; //increase number by 1. You can use myAge = myAge + 1; or myAge += 1;
+myAge +=1;
+
+console.log("My age next year will be " + myAge)
+
+var iLikeCheese = true; // using one "=" is to assign/reassign a variable
+iLikeCheese == true // using two "==" is used for comparisons
